2bff8466602f248f1807242a9d673a107661521d
heiskama <34509523+heiskama@users.noreply.github.com>
2024-09-24 17:49:25 +0300

    Add private ip to outputs

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -2,6 +2,10 @@ output "public_ip" {
   value = azurerm_public_ip.pip.ip_address
 }
 
+output "private_ip" {
+  value = azurerm_network_interface.nic.private_ip_address
+}
+
 output "username" {
   value = var.username
 }
